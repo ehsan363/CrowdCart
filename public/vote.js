ea4375cd6deaa953fcbb7c06abdf69bdb1c685ab
Mohammed Ehsan <mohammedehsan363@gmail.com>
2026-01-07 08:43:48 +0400

    update

--- public/vote.js
+++ public/vote.js
@@ -4,16 +4,6 @@ import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/fi
 
 let currentWeekId = null;
 
-window.addEventListener("DOMContentLoaded", () => {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      loadWeeklyVote();
-    } else {
-      document.getElementById("voteContainer").innerHTML =
-        "<p style='color:white;'>Please login to vote.</p>";
-    }
-  });
-});
 /* ---------------- LOAD WEEKLY PRODUCTS ---------------- */
 
 async function loadWeeklyVote() {
